0b7cb864e75a05707f5a6dba1d62d140366cf26e
Александр <a.gavrilov@mytechhelp.ru>
2025-02-28 00:16:23 +0300

    add tg hook

--- app/bot/bot.py
+++ app/bot/bot.py
@@ -1,6 +1,8 @@
 from aiogram import Dispatcher, Bot
 
+from app.bot.handlers.start import router
 from app.core.config import BOT_TOKEN
 
 bot = Bot(token=str(BOT_TOKEN))
 dp = Dispatcher()
+dp.include_router(router)
--- app/bot/handlers/start.py
+++ app/bot/handlers/start.py
@@ -1,10 +1,10 @@
+from aiogram import Router
 from aiogram.filters import CommandStart
 from aiogram.types import Message
 
-from app.bot.bot import dp
 from app.bot.keyboards.inline import web_app
 
-
-@dp.message(CommandStart())
+router = Router()
+@router.message(CommandStart())
 async def start(message: Message) -> None:
     await message.answer('🎲 Начни играть сейчас', reply_markup=web_app())
